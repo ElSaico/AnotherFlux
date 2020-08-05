84193262e2c11d26a156398c319290a1251180d2
Bruno Marques <bruno.marques@sky.uk>
2020-08-05 15:49:16 +0100

    Yeeted app project and replaced with .NET Core + Avalonia

--- AnotherFlux/Program.cs
+++ AnotherFlux/Program.cs
@@ -1,14 +1,24 @@
-﻿using Gtk;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Logging.Serilog;
+using Avalonia.ReactiveUI;
 
 namespace AnotherFlux
 {
-    class MainClass
+    class Program
     {
-        public static void Main()
-        {
-            Application.Init();
-            new MainWindow();
-            Application.Run();
-        }
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug()
+                .UseReactiveUI();
     }
 }
---
+++ AnotherFlux/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnotherFlux.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "ayyy lmao??!";
+    }
+}
