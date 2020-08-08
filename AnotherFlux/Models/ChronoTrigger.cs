8d2b70701720890ebc38d9f7f1913c8e5df89b11
Bruno Marques <bruno@bmarques.net>
2020-08-09 00:40:36 +0100

    More code unraveled

--- AnotherFlux/Models/ChronoTrigger.cs
+++ AnotherFlux/Models/ChronoTrigger.cs
@@ -15,11 +15,29 @@ namespace AnotherFlux.Models
 
         private byte[] rawData;
         private RomType romType;
+
         private bool expandedRom;
-        private bool allNlz;
-        private bool dactylNlz;
-        private bool startupLoc; // ???
-        private bool betaEvents;
+        private bool patchAllNlz;
+        private bool patchDactylNlz;
+        private bool patchStartLocation;
+        private bool patchBetaEvents;
+        private SaveRecord[] locationMaps;
+        private SaveRecord[] locationTileAssemblyL3;
+        private SaveRecord[] locationTileAssemblyL12;
+        private SaveRecord locationExits;
+        private SaveRecord treasure;
+        private SaveRecord[] overworldPalettes;
+        private SaveRecord[] overworldMaps;
+        private SaveRecord[] overworldTileAssemblyL3;
+        private SaveRecord[] overworldTileAssemblyL12;
+        private SaveRecord[] overworldExits;
+        private SaveRecord[] strings;
+        private SaveRecord[] overworldTileProperties;
+        private SaveRecord[] overworldEvents;
+        private SaveRecord[] overworldMusicTransitionData;
+        private SaveRecord[] customData;
+        private SaveRecord[] locationEventsAndDialogue;
+        private SaveRecord[] substrings;
 
         public static bool IsRomHeadered(FileStream rom)
         {
@@ -27,7 +45,7 @@ namespace AnotherFlux.Models
             {
                 if ((rom.Length % ROM_BLOCK_SIZE) != ROM_HEADER_SIZE)
                 {
-                    throw new ROMReadException("Incorrect ROM size");
+                    throw new RomReadException("Incorrect ROM size");
                 }
                 return true;
             }
@@ -58,7 +76,7 @@ namespace AnotherFlux.Models
                 file.Seek((long)RomAddress.NameInterleaved, SeekOrigin.Begin);
                 if (reader.ReadChars(ROM_NAME_LENGTH).ToString() != ROM_NAME)
                 {
-                    throw new ROMReadException("Incorrect ROM name");
+                    throw new RomReadException("Incorrect ROM name");
                 }
                 return true;
             }
@@ -78,7 +96,7 @@ namespace AnotherFlux.Models
             }
             using (var writer = new BinaryWriter(File.OpenWrite(filename)))
             {
-                for (ushort i = 0; i < nBlocks; i++)
+                for (var i = 0; i < nBlocks; i++)
                 {
                     writer.Write(romData, (int)((i * ROM_BLOCK_SIZE) | ROM_INTERLEAVE_OFFSET), ROM_BLOCK_SIZE);
                     writer.Write(romData, i * ROM_BLOCK_SIZE, ROM_BLOCK_SIZE);
@@ -95,7 +113,7 @@ namespace AnotherFlux.Models
                 file.Seek((long)RomAddress.Version, SeekOrigin.Begin);
                 if (reader.ReadByte() != 0)
                 {
-                    throw new ROMReadException("Invalid ROM version; v1.0 required");
+                    throw new RomReadException("Invalid ROM version; v1.0 required");
                 }
                 file.Seek(0L, SeekOrigin.Begin);
                 rawData = reader.ReadBytes((int)file.Length);
@@ -110,154 +128,153 @@ namespace AnotherFlux.Models
             expandedRom = rawData[(int)RomAddress.MapMode] == ROM_TYPE_EXHIROM;
 
             // TODO check if the logic of these tests is not inverted
-            allNlz = romType == RomType.Beta || rawData[GlobalShared.GetRomAddr(RomAddr.NLZPatch)] != 0x64;
-            dactylNlz = romType == RomType.Beta || rawData[GlobalShared.GetRomAddr(RomAddr.DactylPatch)] == 0x64;
-            startupLoc = romType == RomType.Beta || rawData[0x2E1A] != 0x20;
-            betaEvents = romType == RomType.Beta && rawData[0x372012] != 0xA7;
+            patchAllNlz = romType == RomType.Beta || rawData[GlobalShared.GetRomAddr(RomAddr.NLZPatch)] != 0x64;
+            patchDactylNlz = romType == RomType.Beta || rawData[GlobalShared.GetRomAddr(RomAddr.DactylPatch)] == 0x64;
+            patchStartLocation = romType == RomType.Beta || rawData[0x2E1A] != 0x20;
+            patchBetaEvents = romType == RomType.Beta && rawData[0x372012] != 0xA7;
 
-            // here be dragons
-            GlobalShared.PostStatus("Decompressing Location maps");
-            G.SaveRec[1] = new SaveRecord[256];
-            GetSimpleRec(G.SaveRec[1], 24582u, RomAddr.LocMap);
-            GlobalShared.PostStatus("Decompressing Location Tile Assembly");
-            G.SaveRec[5] = new SaveRecord[87];
-            GetSimpleRec(G.SaveRec[5], 4096u, RomAddr.LocTileAsmL12);
-            G.SaveRec[7] = new SaveRecord[23];
-            GetSimpleRec(G.SaveRec[7], 4096u, RomAddr.LocTileAsmL3);
-            GlobalShared.PostStatus("Getting Location exits");
-            G.SaveRec[9] = new SaveRecord[1];
-            G.SaveRec[9][0] = new SaveRecord();
-            SaveRecord saveRecord = G.SaveRec[9][0];
-            saveRecord.nMaxSize = 1792u;
-            saveRecord.nPointerType = PointerType.SizedByAddress;
-            saveRecord.nRecords = 512u;
-            saveRecord.nRecSize = 7u;
+            locationMaps = new SaveRecord[256];
+            GetSimpleRec(locationMaps, 0x6006, RomAddr.LocMap);
+            locationTileAssemblyL12 = new SaveRecord[87];
+            GetSimpleRec(locationTileAssemblyL12, 0x1000, RomAddr.LocTileAsmL12);
+            locationTileAssemblyL3 = new SaveRecord[23];
+            GetSimpleRec(locationTileAssemblyL3, 0x1000, RomAddr.LocTileAsmL3);
+
+            locationExits = new SaveRecord
+            {
+                nMaxSize = 0x700,
+                nPointerType = PointerType.SizedByAddress,
+                nRecords = 0x200,
+                nRecSize = 7
+            };
             if (romType != RomType.USA)
             {
-                saveRecord.Pointer = new PointerRecord[12];
-                saveRecord.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.LocExit));
-                saveRecord.Pointer[1] = new PointerRecord(0x9CD6L, 0, true, false);
-                saveRecord.Pointer[2] = new PointerRecord(0x9CDEL, 0, true, false);
-                saveRecord.Pointer[3] = new PointerRecord(0x9CE8L, 0, true, false);
-                saveRecord.Pointer[4] = new PointerRecord(0x9CF8L, 0, true, false);
-                saveRecord.Pointer[5] = new PointerRecord(0x9D12L, 0, true, false);
-                saveRecord.Pointer[6] = new PointerRecord(0x9D19L, 0, true, false);
-                saveRecord.Pointer[7] = new PointerRecord(0x9D20L, 0, true, false);
-                saveRecord.Pointer[8] = new PointerRecord(0xA6A6L);
-                saveRecord.Pointer[9] = new PointerRecord(0xA6BBL, 0, true, false);
-                saveRecord.Pointer[10] = new PointerRecord(0xA6C4L, 0, true, false);
-                saveRecord.Pointer[11] = new PointerRecord(0xA6E4L, 0, true, false);
+                locationExits.Pointer = new PointerRecord[12];
+                locationExits.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.LocExit));
+                locationExits.Pointer[1] = new PointerRecord(0x9CD6L, 0, true, false);
+                locationExits.Pointer[2] = new PointerRecord(0x9CDEL, 0, true, false);
+                locationExits.Pointer[3] = new PointerRecord(0x9CE8L, 0, true, false);
+                locationExits.Pointer[4] = new PointerRecord(0x9CF8L, 0, true, false);
+                locationExits.Pointer[5] = new PointerRecord(0x9D12L, 0, true, false);
+                locationExits.Pointer[6] = new PointerRecord(0x9D19L, 0, true, false);
+                locationExits.Pointer[7] = new PointerRecord(0x9D20L, 0, true, false);
+                locationExits.Pointer[8] = new PointerRecord(0xA6A6L);
+                locationExits.Pointer[9] = new PointerRecord(0xA6BBL, 0, true, false);
+                locationExits.Pointer[10] = new PointerRecord(0xA6C4L, 0, true, false);
+                locationExits.Pointer[11] = new PointerRecord(0xA6E4L, 0, true, false);
             }
             else
             {
-                saveRecord.Pointer = new PointerRecord[11];
-                saveRecord.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.LocExit));
-                saveRecord.Pointer[1] = new PointerRecord(0x9538L, 0, true, false);
-                saveRecord.Pointer[2] = new PointerRecord(0x9540L, 0, true, false);
-                saveRecord.Pointer[3] = new PointerRecord(0x954AL, 0, true, false);
-                saveRecord.Pointer[4] = new PointerRecord(0x9564L, 0, true, false);
-                saveRecord.Pointer[5] = new PointerRecord(0x956BL, 0, true, false);
-                saveRecord.Pointer[6] = new PointerRecord(0x9572L, 0, true, false);
-                saveRecord.Pointer[7] = new PointerRecord(0x9FBFL);
-                saveRecord.Pointer[8] = new PointerRecord(0x9FD4L, 0, true, false);
-                saveRecord.Pointer[9] = new PointerRecord(0x9FDDL, 0, true, false);
-                saveRecord.Pointer[10] = new PointerRecord(0x9FFDL, 0, true, false);
+                locationExits.Pointer = new PointerRecord[11];
+                locationExits.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.LocExit));
+                locationExits.Pointer[1] = new PointerRecord(0x9538L, 0, true, false);
+                locationExits.Pointer[2] = new PointerRecord(0x9540L, 0, true, false);
+                locationExits.Pointer[3] = new PointerRecord(0x954AL, 0, true, false);
+                locationExits.Pointer[4] = new PointerRecord(0x9564L, 0, true, false);
+                locationExits.Pointer[5] = new PointerRecord(0x956BL, 0, true, false);
+                locationExits.Pointer[6] = new PointerRecord(0x9572L, 0, true, false);
+                locationExits.Pointer[7] = new PointerRecord(0x9FBFL);
+                locationExits.Pointer[8] = new PointerRecord(0x9FD4L, 0, true, false);
+                locationExits.Pointer[9] = new PointerRecord(0x9FDDL, 0, true, false);
+                locationExits.Pointer[10] = new PointerRecord(0x9FFDL, 0, true, false);
             }
-            saveRecord.nOrigAddr = GlobalShared.GetFileOffset((int)saveRecord.Pointer[0].nByte);
-            saveRecord.Get();
-            GlobalShared.PostStatus("Getting Treasure");
-            G.SaveRec[13] = new SaveRecord[1];
-            G.SaveRec[13][0] = new SaveRecord();
-            saveRecord = G.SaveRec[13][0];
-            saveRecord.nMaxSize = 1024u;
-            saveRecord.nPointerType = PointerType.SizedByAddress;
-            saveRecord.nRecords = 512u;
-            saveRecord.nRecSize = 4u;
+            locationExits.nOrigAddr = GlobalShared.GetFileOffset((int)locationExits.Pointer[0].nByte);
+            locationExits.Get();
+
+            treasure = new SaveRecord
+            {
+                nMaxSize = 0x400,
+                nPointerType = PointerType.SizedByAddress,
+                nRecords = 0x200,
+                nRecSize = 4
+            };
             if (romType != RomType.Beta)
             {
-                saveRecord.Pointer = new PointerRecord[8];
-                saveRecord.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.Treasure));
-                saveRecord.Pointer[1] = new PointerRecord(7707L, 0, true, false);
-                saveRecord.Pointer[2] = new PointerRecord(7771L, 0, true, false);
-                saveRecord.Pointer[3] = new PointerRecord(42827L, 2, true, true);
-                saveRecord.Pointer[4] = new PointerRecord(42833L);
-                saveRecord.Pointer[5] = new PointerRecord(42848L, 0, true, false);
-                saveRecord.Pointer[6] = new PointerRecord(42854L, 0, true, false);
-                saveRecord.Pointer[7] = new PointerRecord(42868L, 0, true, false);
+                treasure.Pointer = new PointerRecord[8];
+                treasure.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.Treasure));
+                treasure.Pointer[1] = new PointerRecord(0x1E1BL, 0, true, false);
+                treasure.Pointer[2] = new PointerRecord(0x1E5BL, 0, true, false);
+                treasure.Pointer[3] = new PointerRecord(0xA74BL, 2, true, true);
+                treasure.Pointer[4] = new PointerRecord(0xA751L);
+                treasure.Pointer[5] = new PointerRecord(0xA760L, 0, true, false);
+                treasure.Pointer[6] = new PointerRecord(0xA766L, 0, true, false);
+                treasure.Pointer[7] = new PointerRecord(0xA774L, 0, true, false);
             }
             else
             {
-                saveRecord.Pointer = new PointerRecord[6];
-                saveRecord.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.Treasure));
-                saveRecord.Pointer[1] = new PointerRecord(8229L, 0, true, false);
-                saveRecord.Pointer[2] = new PointerRecord(8293L, 0, true, false);
-                saveRecord.Pointer[3] = new PointerRecord(41060L);
-                saveRecord.Pointer[4] = new PointerRecord(41068L);
-                saveRecord.Pointer[5] = new PointerRecord(41084L, 0, true, false);
+                treasure.Pointer = new PointerRecord[6];
+                treasure.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.Treasure));
+                treasure.Pointer[1] = new PointerRecord(0x2025L, 0, true, false);
+                treasure.Pointer[2] = new PointerRecord(0x2065L, 0, true, false);
+                treasure.Pointer[3] = new PointerRecord(0xA064L);
+                treasure.Pointer[4] = new PointerRecord(0xA06CL);
+                treasure.Pointer[5] = new PointerRecord(0xA07CL, 0, true, false);
             }
-            saveRecord.nOrigAddr = GlobalShared.GetFileOffset((int)saveRecord.Pointer[0].nByte);
-            saveRecord.Get();
-            GlobalShared.PostStatus("Decompressing Overworld Palettes");
-            OverworldPalette = new SaveRecord[13];
-            GetSimpleRec(OverworldPalette, 512u, RomAddr.OWPal);
-            GlobalShared.PostStatus("Decompressing Overworld maps");
-            G.SaveRec[0] = new SaveRecord[8];
-            GetSimpleRec(G.SaveRec[0], 12288u, RomAddr.OWMap);
-            GlobalShared.PostStatus("Decompressing Overworld Tile Assembly");
-            G.SaveRec[6] = new SaveRecord[6];
-            GetSimpleRec(G.SaveRec[6], 4096u, RomAddr.OWTileAsmL12);
-            G.SaveRec[8] = new SaveRecord[6];
-            GetSimpleRec(G.SaveRec[8], 4096u, RomAddr.OWTileAsmL3);
-            GlobalShared.PostStatus("Decompressing Overworld exits");
-            G.SaveRec[3] = new SaveRecord[8];
-            GetOWExitsRec(G.SaveRec[3], 2816u, RomAddr.OWExit, false);
-            GlobalShared.PostStatus("Decompressing Strings");
-            G.SaveRec[11] = new SaveRecord[15];
-            for (ushort num = 0; num < 15; num = (ushort)(num + 1))
+            treasure.nOrigAddr = GlobalShared.GetFileOffset((int)locationExits.Pointer[0].nByte);
+            treasure.Get();
+
+            overworldPalettes = new SaveRecord[13];
+            GetSimpleRec(overworldPalettes, 0x200, RomAddr.OWPal);
+            overworldMaps = new SaveRecord[8];
+            GetSimpleRec(overworldMaps, 0x3000, RomAddr.OWMap);
+            overworldTileAssemblyL12 = new SaveRecord[6];
+            GetSimpleRec(overworldTileAssemblyL12, 0x1000, RomAddr.OWTileAsmL12);
+            overworldTileAssemblyL3 = new SaveRecord[6];
+            GetSimpleRec(overworldTileAssemblyL3, 0x1000, RomAddr.OWTileAsmL3);
+            overworldExits = new SaveRecord[8];
+            GetOWExitsRec(overworldExits, 0xB00, RomAddr.OWExit, false);
+
+            strings = new SaveRecord[15];
+            for (var i = 0; i < strings.Length; i++)
             {
-                G.SaveRec[11][num] = new StrSaveRecord();
+                strings[i] = new StrSaveRecord();
             }
             StrForm.InitForm();
             GetLocEvents();
             TranslationOpen(false);
-            GlobalShared.PostStatus("Decompressing Overworld Tile Properties");
-            G.SaveRec[10] = new SaveRecord[8];
-            G.GetSimpleRec(G.SaveRec[10], 512u, RomAddr.OWTileProps, true);
-            GlobalShared.PostStatus("Decompressing Overworld Events");
-            G.SaveRec[2] = new OESaveRecord[8];
-            for (ushort num = 0; num < G.SaveRec[2].Length; num = (ushort)(num + 1))
+
+            overworldTileProperties = new SaveRecord[8];
+            G.GetSimpleRec(overworldTileProperties, 0x200, RomAddr.OWTileProps, true);
+
+            overworldEvents = new OESaveRecord[8];
+            for (var i = 0; i < overworldEvents.Length; i++)
             {
-                G.SaveRec[2][num] = new OESaveRecord(this);
-                OESaveRecord obj2 = (OESaveRecord)G.SaveRec[2][num];
-                obj2.nMaxSize = 65536u;
-                obj2.Pointer = new PointerRecord[1];
-                obj2.Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.OWEvent) + num * 3);
-                obj2.bCompressed = true;
-                obj2.Get();
+                overworldEvents[i] = new OESaveRecord(this)
+                {
+                    nMaxSize = 0x10000,
+                    Pointer = new PointerRecord[1],
+                    bCompressed = true
+                };
+                overworldEvents[i].Pointer[0] = new PointerRecord(GlobalShared.GetRomAddr(RomAddr.OWEvent) + i * 3);
+                overworldEvents[i].Get();
             }
-            GlobalShared.PostStatus("Decompressing Overworld Music Transition Data");
-            G.SaveRec[4] = new SaveRecord[8];
-            G.GetSimpleRec(G.SaveRec[4], 3072u, RomAddr.OWMTD, true);
+
+            overworldMusicTransitionData = new SaveRecord[8];
+            G.GetSimpleRec(overworldMusicTransitionData, 0xC00, RomAddr.OWMTD, true);
+
             GlobalShared.PostStatus("Getting Custom Defined Data");
-            for (ushort num = 0; num < CDRec.Count; num = (ushort)(num + 1))
+            for (var i = 0; i < CDRec.Count; i++)
             {
-                CDRec[num].bFullFlux = true;
-                CDRec[num].Get();
+                CDRec[i].bFullFlux = true;
+                CDRec[i].Get();
             }
+
             GlobalShared.PostStatus("Init Music Form");
             MusicForm.FillSongs();
             MusicForm.FillInstruments();
+
             GlobalShared.PostStatus("Getting Misc Settings");
             MSForm.InitForm();
             if (((Control)CDForm).get_Visible())
             {
                 CDForm.InitForm();
             }
-            for (ushort num = 0; num < PlugList.Count; num = (ushort)(num + 1))
+
+            for (var i = 0; i < PlugList.Count; i++)
             {
-                if (!PlugList[num].GetRecords())
+                if (!PlugList[i].GetRecords())
                 {
-                    GlobalShared.PostStatus("Error - " + PlugList[num].sPlugName + " could not load its data.");
+                    GlobalShared.PostStatus("Error - " + PlugList[i].sPlugName + " could not load its data.");
                 }
             }
         }
